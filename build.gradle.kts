bc2cc7533a979100fa8ede2eb32bced5ffeb1cc5
郭靖 <dev@aisolutionspl.com>
2026-04-25 13:15:18 +0200

    chore(deps): upgrade ai-solutions-java-shared 0.0.2 → 0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     testImplementation("io.rest-assured:rest-assured")
 
     // MavenLocal
-    implementation("com.aisolutions:ai-solutions-java-shared:0.0.2")
+    implementation("com.aisolutions:ai-solutions-java-shared:0.0.3")
 
     // Google API Client Libraries
     implementation("com.google.api-client:google-api-client:2.8.0")
